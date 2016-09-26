88611ccd31e9b2d3075af0f051c0e309b9ccd9aa
TheDonsky <nikadon.net@gmail.com>
2016-09-26 12:14:02 +0000

    create context struct with minimal, yet unimplemented interface

---
+++ Project1/context.c
@@ -0,0 +1,17 @@
+#include"context.h"
+
+
+void context_init(context *this){
+	/* NOT YET IMPLEMETED */
+}
+void context_dispose(context *this){
+	/* NOT YET IMPLEMETED */
+}
+
+void context_cpy(const context *from, context *to){
+	/* NOT YET IMPLEMETED */
+}
+
+void context_set_variable(context *this, const char *name, const char *value){
+	/* NOT YET IMPLEMETED */
+}
---
+++ Project1/context.h
@@ -0,0 +1,19 @@
+
+#ifndef __context__
+#define __context__
+
+
+typedef struct{
+	int i // Temporary; may not compile if empty...
+} context;
+
+void context_init(context *this);
+void context_dispose(context *this);
+
+void context_cpy(const context *from, context *to);
+
+void context_set_variable(context *this, const char *name, const char *value);
+
+
+
+#endif
