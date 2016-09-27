ec5fdefbd809115e9026aaf98919c37bffb02a3d
TheDonsky <nikadon.net@gmail.com>
2016-09-27 09:55:30 +0000

    finalize tokenizer struct

--- Project1/tokenizer.c
+++ Project1/tokenizer.c
@@ -1,27 +1,28 @@
 #include"tokenizer.h"
 #include<string.h>
+#include<stdlib.h>
 
 static void free_string_list(const char **list){
 	if (list == NULL) return;
 	int i = 0;
 	while (list[i] != NULL){
-		free(list[i]);
+		free((char*)list[i]);
 		i++;
 	}
-	free(list);
+	free((char**)list);
 }
 
 static char** cpy_string_list(const char **list){
 	if (list == NULL) return NULL;
 	int n = 0;
 	while (list[n] != NULL) n++;
-	char **clone = malloc(sizzeof(char*) * (n + 1));
+	char **clone = malloc(sizeof(char*) * (n + 1));
 	if (clone == NULL) return NULL;
 	int i;
 	for (i = 0; i < n; i++){
 		clone[i] = strdup(list[i]);
 		if (clone[i] == NULL){
-			free_string_list(clone);
+			free_string_list((const char**)clone);
 			return NULL;
 		}
 	}
@@ -29,86 +30,122 @@ static char** cpy_string_list(const char **list){
 	return clone;
 }
 
-static bool string_starts_with_pattern(const char *string, const char *patern){
+static bool string_find_pattern(const char *string, const char *patern){
 	int i = 0;
-	while (patern[i] != NULL){
-		if (string[i] == patern[i]) return false;
+	while (patern[i] != '\0'){
+		if (string[i] != patern[i]) return 0;
 		i++;
 	}
-	return true;
+	return i;
 }
 
-static bool string_starts_with_pattern_from(const char *string, const char **paterns){
+static int string_find_pattern_from(const char *string, const char **paterns, const char **patern){
 	int i = 0;
-	while (patterns[i] != NULL)
-		if (string_starts_with_pattern(string, patterns[i])) return true;
-	return false;
-}
-
-static void tokenizer_lock_tok_end(tokenizer *this){
-	this->end_char = (*this->tok_end);
-	(*this->tok_end) = '/0';
-}
-
-static void tokenizer_free_tok_end(tokenizer *this){
-	(*this->tok_end) = this->end_char;
-	this->tok_start = this->tok_end;
-}
-
-void tokenizer_init_fast(tokenizer *this, char *string, const char **delimiters, const char **ignored_starts, const char **ignored_ends){
-	this->string = string;
-	this->delimiters = delimiters;
-	this->ignored_starts = ignored_starts;
-	this->ignored_ends = ignored_ends;
-	
-	this->tok_start = this->string;
-	this->tok_end = this->string;
-	tokenizer_lock_tok_end(this);
+	while (paterns[i] != NULL){
+		int result = string_find_pattern(string, paterns[i]);
+		if (result > 0){
+			if (patern != NULL) (*patern) = paterns[i];
+			return result;
+		}
+		i++;
+	}
+	return 0;
 }
 
 bool tokenizer_init(tokenizer *this, const char *string, const char **delimiters, const char **ignored_starts, const char **ignored_ends){
-	char *string_cln = NULL;
-	char **delimiters_cln = NULL;
-	char **ignored_starts_cln = NULL;
-	char **ignored_ends_cln = NULL;
+	const char *string_cln = NULL;
+	const char **delimiters_cln = NULL;
+	const char **ignored_starts_cln = NULL;
+	const char **ignored_ends_cln = NULL;
+	char *token = NULL;
 
 	string_cln = strdup(string);
 	bool success = (string_cln != NULL);
-	if (success){ delimiters_cln = cpy_string_list(delimiters); success = (delimiters_cln != NULL); }
-	if (success){ ignored_starts_cln = cpy_string_list(ignored_starts); success = (ignored_starts_cln != NULL); }
-	if (success){ ignored_ends_cln = cpy_string_list(ignored_ends); success = (ignored_ends_cln != NULL); }
+	if (success){ delimiters_cln = (const char**)cpy_string_list(delimiters); success = (delimiters_cln != NULL); }
+	if (success){ ignored_starts_cln = (const char**)cpy_string_list(ignored_starts); success = (ignored_starts_cln != NULL); }
+	if (success){ ignored_ends_cln = (const char**)cpy_string_list(ignored_ends); success = (ignored_ends_cln != NULL); }
+	if (success){ token = strdup(string); success = (token != NULL); }
 	if (!success){
-		if (string_cln != NULL) free(string_cln);
+		if (string_cln != NULL) free((char*)string_cln);
 		free_string_list(delimiters_cln);
 		free_string_list(ignored_starts_cln);
 		free_string_list(ignored_ends_cln);
+		if (token != NULL) free(token);
 		return false;
 	}
 	else{
-		tokenizer_init_fast(this, string_cln, delimiters_cln, ignored_starts_cln, ignored_ends_cln);
+		this->string = string_cln;
+		this->delimiters = delimiters_cln;
+		this->ignored_starts = ignored_starts_cln;
+		this->ignored_ends = ignored_ends_cln;
+
+		this->cursor = this->string;
+		this->current_token = token;
+		this->last_delimiter = NULL;
 		return true;
 	}
 }
 
 void tokenizer_dispose(tokenizer *this){
-	free(this->string);
-	freeStringList(this->delimiters);
-	freeStringList(this->ignored_starts);
-	freeStringList(this->ignored_ends);
+	free((char*)this->string);
+	free_string_list(this->delimiters);
+	free_string_list(this->ignored_starts);
+	free_string_list(this->ignored_ends);
+	free(this->current_token);
 }
 
-
+static bool tokenizer_jump_over_patterns_from(tokenizer *this, const char **patterns){
+	while (true){
+		if ((*this->cursor) == '\0') return false;
+		int jump = string_find_pattern_from(this->cursor, patterns, NULL);
+		if (jump > 0) this->cursor += jump;
+		else return true;
+	}
+}
 
 bool tokenizer_move_to_next(tokenizer *this){
-	tokenizer_free_tok_end(this);
-	tokenizer_lock_tok_end(this);
-	return true; // TMP...
+	char *token = this->current_token;
+	this->last_delimiter = NULL;
+	tokenizer_jump_over_patterns_from(this, this->delimiters);
+	while (true){
+		if ((*this->cursor) == '\0') break;
+		const char *delimiter = NULL;
+		string_find_pattern_from(this->cursor, this->delimiters, &delimiter);
+		if (delimiter != NULL){
+			this->last_delimiter = delimiter;
+			break;
+		}
+		int ignored = string_find_pattern_from(this->cursor, this->ignored_starts, NULL);
+		if (ignored > 0){
+			this->cursor += ignored;
+			while (true){
+				if ((*this->cursor) == '\0') break;
+				int jump = string_find_pattern_from(this->cursor, this->ignored_ends, NULL);
+				if (jump > 0){
+					this->cursor += jump;
+					break;
+				}
+				else this->cursor++;
+			}
+			tokenizer_jump_over_patterns_from(this, this->delimiters);
+		}
+		else{
+			(*token) = (*this->cursor);
+			token++;
+			this->cursor++;
+		}
+	}
+	(*token) = '\0';
+	return (token != this->current_token);
 }
 char* tokenizer_get_next_token(tokenizer *this){
 	if (tokenizer_get_current_token(this))
-		return tokenizer_get_current_token(this);
+		return this->current_token;
 	else return NULL;
 }
 char* tokenizer_get_current_token(tokenizer *this){
-	return this->tok_start;
+	return this->current_token;
+}
+const char *tokenizer_get_last_delimiter(tokenizer *this){
+	return this->last_delimiter;
 }
--- Project1/tokenizer.h
+++ Project1/tokenizer.h
@@ -3,31 +3,22 @@
 #define __tokenizer__
 
 typedef struct{
-	char *string;
+	const char *string;
 	const char **delimiters;
 	const char **ignored_starts;
 	const char **ignored_ends;
-	char *tok_start, *tok_end, end_char;
+	const char *cursor;
+	char *current_token;
+	const char *last_delimiter;
 } tokenizer;
-
-/** Initializes tokenizer fast, without copying string or delimiters, 
-	temporarily altering input when parsing. 
-	Notes:
-		1. May be used if and only if the whole text is not needed while working on tokens;
-		2. delimiters, ignored_starts and ignored_ends sizes are determined
-			from the number of strings in them before encountering NULL
-		3. tokenizer_dispose() function SHOULD NOT be called on a tokenizer constructed with this constructor.
-*/
-void tokenizer_init_fast(tokenizer *this, char *string, const char **delimiters, const char **ignored_starts, const char **ignored_ends);
 
 /** Initializes given tokenizer;
 	Notes:
-		1. This constructor copies both the string and the delimiters on heap,
-			making the structure most reliable for any case, but also a little slower;
+		1. Constructor copies both the string and the delimiters on heap
+			(so no worries aboud data being damaged or something);
 		2. delimiters, ignored_starts and ignored_ends sizes are determined
 			from the number of strings in them before encountering NULL
-		3. tokenizer constructed with this function requires calling tokenizer_dispose() function.
-*/
+		3. tokenizer constructed with this function requires calling tokenizer_dispose() function. */
 bool tokenizer_init(tokenizer *this, const char *string, const char **delimiters, const char **ignored_starts, const char **ignored_ends);
 
 /** Disposes of the tokenizer */
@@ -35,14 +26,22 @@ void tokenizer_dispose(tokenizer *this);
 
 
 /** Returns true, if the tokenizer can give us one more token
-	Note: should be called before retrieving the first token.
-*/
+	Note: should be called before retrieving the first token. */
 bool tokenizer_move_to_next(tokenizer *this);
-/** Moves to the next token and returns it (or NULL if unable) */
+
+/** Moves to the next token and returns it (or NULL if unable)
+	Note: View notes for tokenizer_get_next_token(). */
 char* tokenizer_get_next_token(tokenizer *this);
+
 /** Returns current token (or empty string, if unable)
-	Note: returned token is not copied/reallocated and altering it may effect internal logic of the tokenizer.
-*/
+	Notes:
+		1. Returned token is not copied/reallocated by the tokenizer
+			and will change to the next token after calling tokenizer_move_to_next() function;
+		2. Freeing the returned value will cause undefined behaviour. */
 char* tokenizer_get_current_token(tokenizer *this);
+
+/** Returns last delimiter (NULL if end of line)
+	Note: no need for free. */
+const char *tokenizer_get_last_delimiter(tokenizer *this);
 
 #endif
