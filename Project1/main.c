54599e16ebfef83f2fcc8fedcaf77ad884971111
TheDonsky <nikadon.net@gmail.com>
2016-09-29 14:53:51 +0000

    implement main.c

--- Project1/main.c
+++ Project1/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <signal.h>
 #include "bool.h"
+#include "input_parser.h"
 
 void ignore (int sig){
     // do nothing
@@ -11,12 +12,17 @@ int main() {
         signal(SIGINT, ignore);
     if (signal(SIGTSTP, SIG_IGN) != SIG_IGN)
         signal(SIGINT, ignore);
-    while(true){
-
-
-
-    }
-
-
+	printf("Shell started\n");
+	context con;
+	context_init(&con);
+	while (true){
+		char *line = readline("$ ");
+		if (line == NULL) break;
+		add_history(line);
+		if (strcmp(line, "exit") == 0) break;
+		parse_input_line(line, &con);
+		free(line);
+	}
+	context_dispose(&con);
     return 0;
 }
