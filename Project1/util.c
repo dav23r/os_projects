efc878cd1c0c6f5bcbde9dddb3798bde1532481d
dav23r <dav23r@gmail.com>
2016-09-29 13:52:57 +0400

    Add path checker for program name
    traverses environment variable and
    checks if the given program exists
    in any of the given folder options

--- Project1/util.c
+++ Project1/util.c
@@ -59,11 +59,25 @@ const string_pair ESCAPE_SEQUENCES[] = {
 
 
 
-
-
-
-
-
-
-
-#endif //PROJECT1_UTIL_C_H
+bool get_path_of_program(char * program_name, char * res){
+    const char* s = getenv("PATH");
+    tokenizer tok;
+    const char * delims[] = {":",DELIMITER_END};
+    const string_pair empty[] = { STRING_PAIR_END };
+    tokenizer_init(&tok,s,delims,empty, empty);
+    while(tokenizer_move_to_next(&tok)){
+        char * t = tokenizer_get_current_token(&tok);
+        char * p = malloc(strlen(t)+strlen(program_name)+1);
+        p[0]='\0';
+        strcat(p,t);
+        strcat(p,program_name);
+        struct stat sb;
+        if (stat(p, &sb) == 0 && S_ISDIR(sb.st_mode)){
+            res = p;
+            tokenizer_dispose(&tok);
+            return true;
+        }
+    }
+    tokenizer_dispose(&tok);
+    return false;
+}
