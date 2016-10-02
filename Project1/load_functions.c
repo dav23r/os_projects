0b389ab3406a1167dee9c2ffc52edb47c10addce
BekaMaisuradze <fcb10lal24@gmail.com>
2016-10-02 17:43:52 +0400

    Load 'cd' and '?' functions

--- Project1/load_functions.c
+++ Project1/load_functions.c
@@ -8,10 +8,11 @@ void load_functions(hashset *map) {
 
 	storeFuncIntoMap(map, "fsh_kill",		fsh_kill);
 	storeFuncIntoMap(map, "fsh_nice",		fsh_nice);
-	storeFuncIntoMap(map, "fsh_cd",			fsh_cd);
+	storeFuncIntoMap(map, "cd",				fsh_cd);
 	storeFuncIntoMap(map, "fsh_pwd",		fsh_pwd);
 	storeFuncIntoMap(map, "echo",			fsh_echo);
 	storeFuncIntoMap(map, "export",			fsh_echo_export);
+	storeFuncIntoMap(map, "?",				fsh_info);
 }
 
 void storeFuncIntoMap(hashset *map, char *fname, func_pointer fn) {
