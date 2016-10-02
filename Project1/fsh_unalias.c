8035724eec412da6522fc0272bb40f30735fcf05
dav23r <dav23r@gmail.com>
2016-10-02 16:38:46 +0400

    Change interface of some built-ins

--- Project1/fsh_unalias.c
+++ Project1/fsh_unalias.c
@@ -1,20 +1,26 @@
 #include "fsh_unalias.h"
 
+// Implementation of 'unalias' shell built-in
 bool fsh_unalias(pos_arguments *args, context *context){
+
 	if (args->num_args != 1) {
 		printf("Too much arguments, only alias name should be given\n");
 		return false;
 	}
+
 	char *alias = *(args->arguments);
 	if (strlen(alias) == 0) {
 		printf("Empty alias name given\n");
 		return false;
 	}	
+
+	// Remove alias entry from hashset
 	hashset *aliases_set = context->aliases;
 	if (HashSetLookup(aliases_set, alias) == NULL){
-		printf("Alias not found");
+		printf("Alias not found\n");
 		return false;
 	}
+
 	HashSetDelete(aliases_set, &alias);
 	return true;
 }
