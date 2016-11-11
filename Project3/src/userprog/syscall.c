1b89d967542622d99d4761448f3f61be3a1483e4
David Akopov <dav23r@gmail.com>
2016-11-11 08:19:28 +0000

    Add pointer validation mechanisms

--- Project3/src/userprog/syscall.c
+++ Project3/src/userprog/syscall.c
@@ -3,6 +3,36 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "userprog/pagedir.h"
+#include "userprog/tss.h"
+#include "userprog/process.h"
+#include "threads/vaddr.h"
+
+
+// Checks, if the user address is valid (by making sure, it's below PHYS_BASE and it's already mapped/allocated)
+static int user_address_valid(void *addr) {
+	if (addr == NULL || (!is_user_vaddr((uint32_t*)addr))) return false;
+	else return (pagedir_get_page(thread_current()->pagedir, (uint32_t *)addr) != NULL);
+}
+
+// Checks, if the given number of pointers in a row are valid (returns 0 even if one of them is not)
+static int pointers_valid(const void *address, uint32_t count) {
+	const char *addr = (const char*)address;
+	uint32_t i;
+	for (i = 0; i < count; i++)
+		if (!user_address_valid((void*)(addr + i))) return 0;
+	return 1;
+}
+
+// Checks, if the given string is valid, by moving along it and verifying every single byte, stopping at '\0'.
+static bool string_valid(const char *address) {
+	while (true) {
+		if (!user_address_valid((void*)address)) return false;
+		if ((*address) == '\0') return true;
+		address++;
+	}
+}
+
 
 static void syscall_handler (struct intr_frame *);
 
