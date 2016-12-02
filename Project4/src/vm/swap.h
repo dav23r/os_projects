975d8b108dcfa16185218fff7c2464c72a8445e5
TheDonsky <nikadon.net@gmail.com>
2016-12-02 09:59:37 +0000

    Create swap table prototypes

---
+++ Project4/src/vm/swap.h
@@ -0,0 +1,13 @@
+#ifndef SWAP_H
+#define SWAP_H
+
+typedef long long swap_page;
+#define SWAP_NO_PAGE -1
+
+swap_page swap_get_page(void);
+void swap_free_page(swap_page page);
+void swap_load_page_to_ram(swap_page page, void *addr);
+void swap_load_page_to_swap(swap_page page, void *addr);
+
+
+#endif
