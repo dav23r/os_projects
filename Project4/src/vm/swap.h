9fbed8e1587b0870e97b8980ba88afce82bb6d63
TheDonsky <nikadon.net@gmail.com>
2016-12-05 08:33:45 +0000

    Make swap_init() public; add a call to it in init.c

--- Project4/src/vm/swap.h
+++ Project4/src/vm/swap.h
@@ -4,10 +4,11 @@
 typedef long long swap_page;
 #define SWAP_NO_PAGE -1
 
+void swap_init(void);
+
 swap_page swap_get_page(void);
 void swap_free_page(swap_page page);
 void swap_load_page_to_ram(swap_page page, void *addr);
 void swap_load_page_to_swap(swap_page page, void *addr);
-
 
 #endif
