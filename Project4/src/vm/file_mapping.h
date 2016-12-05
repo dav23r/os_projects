fca9c625b64a8ee6c616231d56edebb77d2f0c2d
BekaMaisuradze <fcb10lal24@gmail.com>
2016-12-05 10:36:43 +0000

    Define basic interface for managing existing file mappings

---
+++ Project4/src/vm/file_mapping.h
@@ -0,0 +1,25 @@
+#ifndef FILE_MAPPING_H
+#define FILE_MAPPING_H
+#include "filesys/file.h"
+#include "threads/thread.h"
+
+typedef struct file_mapping {
+	struct file *fl;
+	void *start_vaddr;
+};
+
+void file_mapping_init(struct file_mapping *f);
+void file_mapping_dispose(struct file_mapping *f);
+
+typedef struct file_mappings {
+	struct file_mapping *mappings;
+	int pool_size;
+};
+
+void file_mappings_init(struct file_mappings *m);
+void file_mappings_dispose(struct file_mappings *m);
+
+int file_mappings_map(struct thread *t, struct file *fl, void *vaddr);
+int file_mappings_unmap(struct thread *t, int mapping_id);
+
+#endif;
