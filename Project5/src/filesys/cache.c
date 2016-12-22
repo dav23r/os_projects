0cdc671398a3a92a6c357b485c219dc407ea8fee
BekaMaisuradze <fcb10lal24@gmail.com>
2016-12-22 11:33:32 +0000

    Implement basic cache code

--- Project5/src/filesys/cache.c
+++ Project5/src/filesys/cache.c
@@ -1,11 +1,53 @@
 #include "cache.h"
+#include "threads/malloc.h"
 #include "devices/block.h"
 
 
 static struct cache cache;
 
-void cache_init(void) {
-	cache.sectors = NULL;
-	cache.sector_count = 0;
+static unsigned sectors_hash(const struct hash_elem *e, void *aux)
+{
+	ASSERT(!aux);
+	struct sector *sec = hash_entry(e, struct sector, hash_elem);
+	return sec->index;
 }
 
+static bool sectors_map_less(const struct hash_elem *a, const struct hash_elem *b, void *aux) {
+	ASSERT(!aux);
+	struct sector *sector_a = hash_entry(a, struct sector, hash_elem);
+	struct sector *sector_b = hash_entry(b, struct sector, hash_elem);
+	return ((unsigned)sector_a->index < (unsigned)sector_b->index);
+}
+
+void sector_init(struct sector *sec)
+{
+	lock_init(&sec->sector_lock);
+}
+
+void cache_init(void) {
+	lock_init(&cache.cache_lock);
+	sema_init(&cache.cache_sem, 64);
+	hash_init(&cache.index_sectors_map, sectors_hash, sectors_map_less, NULL);
+}
+
+
+struct sector * take_sector(block_sector_t index)
+{
+	lock_acquire(&cache.cache_lock);
+	static struct sector element;
+	element.index = index;
+	struct hash_elem * elem = hash_find(&cache.index_sectors_map, &element.hash_elem);
+	if (elem == NULL) return NULL;
+	struct sector *res = hash_entry(elem, struct sector, hash_elem);
+	lock_release(&cache.cache_lock);
+
+	lock_acquire(&res->sector_lock);
+	return res;
+}
+
+void release_sector(struct sector *sec, block_sector_t index, bool changed)
+{
+	if (changed)
+		cache.sectors[index].dirty = true;
+	lock_release(&sec->sector_lock);
+}
--- Project5/src/filesys/cache.h
+++ Project5/src/filesys/cache.h
@@ -2,16 +2,31 @@
 #ifndef CACHE_H
 #define CACHE_H
 #include "lib/stdbool.h"
+#include "devices/block.h"
+#include "threads/synch.h"
+#include "lib/kernel/hash.h"
+
+#define SECTOR_COUNT 64
 
 struct sector {
-	void *roja;
+	block_sector_t index;
+	char data[BLOCK_SECTOR_SIZE];
 	bool dirty;
+	struct lock sector_lock;
+	struct hash_elem hash_elem;
 };
 struct cache {
-	struct sector *sectors;
-	int sector_count;
+	struct sector sectors[SECTOR_COUNT];
+	struct lock cache_lock;
+	struct semaphore cache_sem;
+	struct hash index_sectors_map;
 };
 
+void sector_init(struct sector *sec);
 void cache_init(void);
+
+struct sector * take_sector(block_sector_t index);
+
+void release_sector(struct sector *sec, block_sector_t index, bool changed);
 
 #endif
