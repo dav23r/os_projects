63eff093a437c7b0d0a47f3e1d43f5fc15284052
BekaMaisuradze <fcb10lal24@gmail.com>
2016-12-22 13:19:45 +0000

    Complete synchronisation logic

--- Project5/src/filesys/cache.c
+++ Project5/src/filesys/cache.c
@@ -1,18 +1,18 @@
 #include "cache.h"
 #include "threads/malloc.h"
 #include "devices/block.h"
+#include "filesys/filesys.h"
 
 
 static struct cache cache;
 
-static unsigned sectors_hash(const struct hash_elem *e, void *aux)
+static unsigned sectors_hash(const struct hash_elem *e, void *aux UNUSED)
 {
-	ASSERT(!aux);
 	struct sector *sec = hash_entry(e, struct sector, hash_elem);
 	return sec->index;
 }
 
-static bool sectors_map_less(const struct hash_elem *a, const struct hash_elem *b, void *aux) {
+static bool sectors_map_less(const struct hash_elem *a, const struct hash_elem *b, void *aux UNUSED) {
 	ASSERT(!aux);
 	struct sector *sector_a = hash_entry(a, struct sector, hash_elem);
 	struct sector *sector_b = hash_entry(b, struct sector, hash_elem);
@@ -21,15 +21,28 @@ static bool sectors_map_less(const struct hash_elem *a, const struct hash_elem *
 
 void sector_init(struct sector *sec)
 {
+	sec->index = (block_sector_t)(-1);
+	sec->owners = 0;
 	lock_init(&sec->sector_lock);
+	lock_init(&sec->owners_lock);
 }
 
 void cache_init(void) {
+	unsigned int i;
+	for (i = 0; i < SECTOR_COUNT; i++) {
+		sector_init(cache.sectors + i);
+		list_push_back(&cache.free_list, &cache.sectors[i].list_elem);
+	}
 	lock_init(&cache.cache_lock);
-	sema_init(&cache.cache_sem, 64);
+	sema_init(&cache.cache_sem, SECTOR_COUNT);
 	hash_init(&cache.index_sectors_map, sectors_hash, sectors_map_less, NULL);
+	list_init(&cache.free_list);
 }
 
+
+static void evict_sector(void) {
+
+}
 
 struct sector * take_sector(block_sector_t index)
 {
@@ -37,8 +50,30 @@ struct sector * take_sector(block_sector_t index)
 	static struct sector element;
 	element.index = index;
 	struct hash_elem * elem = hash_find(&cache.index_sectors_map, &element.hash_elem);
-	if (elem == NULL) return NULL;
-	struct sector *res = hash_entry(elem, struct sector, hash_elem);
+	if (elem != NULL) {
+		struct sector *res = hash_entry(elem, struct sector, hash_elem);
+		
+		lock_acquire(&res->owners_lock);
+		if(res->owners == 0)
+			sema_down(&cache.cache_sem);
+		res->owners++;
+		lock_release(&res->owners_lock);
+
+		lock_release(&cache.cache_lock);
+		lock_acquire(&res->sector_lock);
+		return res;
+	}
+
+	sema_down(&cache.cache_sem);
+
+	if (list_empty(&cache.free_list)) evict_sector();
+	struct sector *res = list_entry(list_pop_back(&cache.free_list), struct sector, list_elem);
+	res->index = index;
+	res->dirty = false;
+	res->owners = 1;
+	block_read(fs_device, index, res->data);
+	hash_insert(&cache.index_sectors_map, &res->hash_elem);
+	
 	lock_release(&cache.cache_lock);
 
 	lock_acquire(&res->sector_lock);
@@ -47,7 +82,15 @@ struct sector * take_sector(block_sector_t index)
 
 void release_sector(struct sector *sec, block_sector_t index, bool changed)
 {
+	ASSERT(sec->index == index);
 	if (changed)
-		cache.sectors[index].dirty = true;
+		sec->dirty = true;
+
+	lock_acquire(&sec->owners_lock);
+	sec->owners--;
+	if (sec->owners == 0)
+		sema_up(&cache.cache_sem);
+	lock_release(&sec->owners_lock);
+
 	lock_release(&sec->sector_lock);
 }
--- Project5/src/filesys/cache.h
+++ Project5/src/filesys/cache.h
@@ -12,14 +12,18 @@ struct sector {
 	block_sector_t index;
 	char data[BLOCK_SECTOR_SIZE];
 	bool dirty;
+	uint32_t owners;
 	struct lock sector_lock;
+	struct lock owners_lock;
 	struct hash_elem hash_elem;
+	struct list_elem list_elem;
 };
 struct cache {
 	struct sector sectors[SECTOR_COUNT];
 	struct lock cache_lock;
 	struct semaphore cache_sem;
 	struct hash index_sectors_map;
+	struct list free_list;
 };
 
 void sector_init(struct sector *sec);
