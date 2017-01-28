b3ba194c78fe69ba8b3badb3eafc0fc4fb81a652
dav23r <dav23r@gmail.com>
2017-01-28 22:32:03 +0400

    Add skeletor for cgi

--- Project6/cgi_runner.h
+++ Project6/cgi_runner.h
@@ -11,8 +11,10 @@
       will be directed there too. It should be stripped of header
       and point to very first line of body.
    -> absolute path of cgi-script to be run.
+   Return value signifies successful evioroment setup and scipt call.
+   It is script's responsibility write valid things to socket.
 */
 
-void run_cgi_script(header_info *http_header, int socket_fd, char *program_to_run);
+bool run_cgi_script(header_info *http_header, int socket_fd, char *program_to_run);
 
 #endif
