e926e1542918710a1f2f30445c9d99198b640fe9
dav23r <dav23r@gmail.com>
2017-01-28 20:26:24 +0400

    Set up cgi calling interface

---
+++ Project6/cgi_runner.h
@@ -0,0 +1,18 @@
+#ifndef CGI_RUNNER
+#define CGI_RUNNED
+
+#include "worker.h"
+
+
+/* Interface to run cgi-script. 
+   -> header_info should contain appropriately assigned values from
+      http header. 
+   -> socked_fd should contain http body content, script's response 
+      will be directed there too. It should be stripped of header
+      and point to very first line of body.
+   -> absolute path of cgi-script to be run.
+*/
+
+void run_cgi_script(header_info *http_header, int socket_fd, char *program_to_run);
+
+#endif
