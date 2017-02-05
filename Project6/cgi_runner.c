1afa1ed0ddf85f5c9aacce034fe22752e086dc52
dav23r <dav23r@gmail.com>
2017-02-05 05:49:03 +0400

    Finish OS

--- Project6/cgi_runner.c
+++ Project6/cgi_runner.c
@@ -3,8 +3,9 @@
 #include "unistd.h"
 #include <assert.h>
 #include "url_tools.h"
+#include <sys/sendfile.h>
 
-void set_up_environment(struct header_info *http_header, 
+void set_up_environment(struct header_info *http_header,
                         struct config *conf);
 
 #define child ((pid_t) 0)
@@ -13,7 +14,7 @@ void set_up_environment(struct header_info *http_header,
 #define stdout_fd 1
 #define UNREACHEBLE false
 
-bool run_cgi_script(struct header_info *http_header, 
+bool run_cgi_script(struct header_info *http_header,
                     int socket_fd, struct config *conf){
 
     char *program_to_run = http_header->requested_objname;
@@ -31,12 +32,10 @@ bool run_cgi_script(struct header_info *http_header,
         perror ("Can't fork!");
         return false;
     } else if (after_fork_fd == child) {
-        /* Free current resourses associated with stdio/stdout. */
-        close(stdout_fd);
-        close(stdin_fd);
         /* Point both of them to the resourse socket_fd points to. */
-        dup2(socket_fd, stdout_fd);
-        dup2(socket_fd, stdin_fd);
+        assert (dup2(socket_fd, stdout_fd) != -1);
+        assert (dup2(socket_fd, stdin_fd) != -1);
+
         set_up_environment(http_header, conf);
         execl(program_to_run, "cgi-script",  NULL);
         /* Later code will be executed only in case of error ocuring. */
@@ -44,7 +43,8 @@ bool run_cgi_script(struct header_info *http_header,
         return false;
     } else {
     /* Parent process waits for child to terminate. */
-        return (wait(NULL) != -1);        // There is only one child 
+        int status = wait(NULL);    // There is only one child
+        return (status != -1);
     }
 
     return UNREACHEBLE;
@@ -54,31 +54,31 @@ bool run_cgi_script(struct header_info *http_header,
    This should happend in child process(after fork), which
    will be followed by jumping to script code block. */
 #define do_overwrite 1
-void set_up_environment(struct header_info *http_header, 
+void set_up_environment(struct header_info *http_header,
                         struct config *conf) {
-     
+
     setenv("GATEWAY_INTERFACE", "CGI/1.1", do_overwrite);
     setenv("SERVER_PROTOCOL", "INCLUDED", do_overwrite);
     setenv("SERVER_NAME", conf->vhost, do_overwrite);
     setenv("SERVER_PORT", conf->port, do_overwrite);
 
     setenv("DOCUMENT_ROOT", conf->document_root, do_overwrite);
-    setenv("REQUEST_METHOD", 
+    setenv("REQUEST_METHOD",
           (http_header->method == GET) ? "GET" : "POST",  do_overwrite);
     setenv("PATH_INFO", http_header->path_info, do_overwrite);
-    setenv("QUERY_STRING", http_header->query_string, do_overwrite);
+    setenv("QUERY_STRING", http_header->query_string == NULL ? "" : http_header->query_string, do_overwrite);
 
     /* Path translated constitues of url encoded PATH_INFO appended to doc-root. */
     int len_path_info = strlen(http_header->path_info);
     int len_doc_root  = strlen(conf->document_root);
     char buff[len_doc_root + len_path_info + 1];
     strcpy(buff, conf->document_root);
-    assert( uri_to_url(http_header->path_info, 
+    assert( uri_to_url(http_header->path_info,
                        buff + len_doc_root,
-                       len_path_info));
+                       len_path_info) );
+
     setenv("PATH_TRANSLATED", buff, do_overwrite);
 
-    setenv("CONTENT_TYPE", http_header->content_type, do_overwrite);
-    setenv("CONTENT_LENGTH", http_header->content_length, do_overwrite);
-
+    setenv("CONTENT_TYPE", http_header->content_type == NULL ? "" : http_header->content_type, do_overwrite);
+    setenv("CONTENT_LENGTH", http_header->content_length == NULL ? "" : http_header->content_length, do_overwrite);
 }
